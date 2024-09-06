c51270ab76ea8d264fa2040a5dc0403c8ae5c76d
FoXiasty <79474591+FoXiasty@users.noreply.github.com>
2024-09-06 12:49:57 +0200

    first commit

---
+++ lekcja 1/kiedy.js
@@ -0,0 +1,2 @@
+const zaIleLat = 21
+console.log("Pierwszy milion zarobię za" , zaIleLat , "lat.");
---
+++ lekcja 1/operatory.js
@@ -0,0 +1,15 @@
+const x = 5;
+
+//dodawanie, odejmowanie, mnożenie, dzielenie
+console.log('52', x + 2);
+console.log('51', x-1);
+console.log('53', x * 3);
+console.log('5/2', x/2);
+
+// modulo czyli reszta z dzielenia
+console.log('5 mod 2', x2);
+console.log('5 mod 9', x9);
+
+//** potęgowanie
+console.log('5^2=', x** 2);
+console.log('5^3=', x ** 3);
