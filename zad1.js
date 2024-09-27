46730d086de5dc9834c01bfac52fd13c9138aab3
FoXiasty <79474591+FoXiasty@users.noreply.github.com>
2024-09-27 13:04:14 +0200

    afaz

---
+++ zad1.js
@@ -0,0 +1,7 @@
+const dae_element = document.querySelector("#dae");
+
+const dataset = dae_element.dataset;
+const attributes = dae_element.attributes;
+
+const output_div = document.querySelector("#cuher");
+
