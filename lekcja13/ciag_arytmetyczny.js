0126d896ad6fc1f8867e2bf3478d6b25dd0371e2
FoXiasty <79474591+FoXiasty@users.noreply.github.com>
2024-10-30 13:04:24 +0100

    qewq

---
+++ lekcja13/ciag_arytmetyczny.js
@@ -0,0 +1,12 @@
+const a1 = document.querySelector("#a1");
+const a2 = document.querySelector("#a2");
+const a3 = document.querySelector("#a3");
+
+document.querySelector().addEventListener("click", () => {
+    if(a3.value >=1) 
+    if(a3.value >=2)
+    if(a3.value >=3)
+    if(a3.value >=1)
+    if(a3.value >=1)
+
+})
