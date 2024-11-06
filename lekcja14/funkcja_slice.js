18a2e541128a1733df0d4cbbf9d8214ad41e4721
FoXiasty <79474591+FoXiasty@users.noreply.github.com>
2024-11-06 14:38:15 +0100

    dfhndz

---
+++ lekcja14/funkcja_slice.js
@@ -0,0 +1,15 @@
+const p1 = document.querySelector("#p1");
+const p2 = document.querySelector("#p2");
+const p3 = document.querySelector("#p3");
+const baton1 = document.querySelector("#baton1");
+const baton2 = document.querySelector("#baton2");
+
+baton1.addEventListener("click", () => {
+let cut1 = p1.textContent.slice(26,36);
+p2.textContent = cut1
+})
+
+baton2.addEventListener("click", () => {
+let cut2 = p1.textContent.slice(-12,-5);
+p3.textContent = cut2;
+})
