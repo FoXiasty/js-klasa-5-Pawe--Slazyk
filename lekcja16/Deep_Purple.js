02a032d1af130ee717c8571434bfd1134c41096a
FoXiasty <79474591+FoXiasty@users.noreply.github.com>
2024-11-15 13:02:50 +0100

    asfavz

---
+++ lekcja16/Deep_Purple.js
@@ -0,0 +1,30 @@
+const baton1 = document.querySelector("#baton1");
+const baton2 = document.querySelector("#baton2");
+const baton3 = document.querySelector("#baton3");
+const baton4 = document.querySelector("#baton4");
+const baton5 = document.querySelector("#baton5");
+
+baton1.addEventListener("click", () => {
+    let concat1 = p1.textContent.concat("Ritchie Blackmore,");
+    p1.textContent = concat1
+    })
+
+baton2.addEventListener("click", () => {
+    let concat2 = p1.textContent.concat("Jon Lord,");
+    p1.textContent = concat2
+    })
+
+baton3.addEventListener("click", () => {
+    let concat3 = p1.textContent.concat("Nick Simper,");
+    p1.textContent = concat3
+    })
+
+baton4.addEventListener("click", () => {
+    let concat4 = p1.textContent.concat("Ian Paice,");
+    p1.textContent = concat4
+    })
+
+baton5.addEventListener("click", () => {
+    let concat5 = p1.textContent.concat("Rod Evans.");
+    p1.textContent = concat5
+    })
