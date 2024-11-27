5b8ad28eed8fea0bbd49556895b41177b369f5a2
FoXiasty <79474591+FoXiasty@users.noreply.github.com>
2024-11-27 14:39:58 +0100

    iuoayefhwul

---
+++ projekt/index.js
@@ -0,0 +1,32 @@
+console.log("jestem tu")
+function validateForm() {
+    let isValid = true;
+
+    const pesel = document.getElementById("pesel");
+    const imie = document.getElementById("imie");
+    const nazwisko = document.getElementById("nazwisko");
+    const kolor = document.getElementsByName("kolor_oczu");
+
+    pesel.style.backgroundColor = "";
+    imie.style.backgroundColor = "";
+    nazwisko.style.backgroundColor = "";
+    kolor.forEach(radio => radio.style.outline = "");
+
+    if (pesel.value.trim() === "" || pesel.value.length !== 11) {
+        console.log("dsfsdfsd")
+        pesel.style.backgroundColor = "lightcoral";
+        isValid = false;
+    }
+
+    if (imie.value.trim() === "") {
+        imie.style.backgroundColor = "lightcoral";
+        isValid = false;
+    }   
+
+    if (nazwisko.value.trim() === "") {
+        nazwisko.style.backgroundColor = "lightcoral";
+        isValid = false;
+    }
+    
+    return isValid;
+}
